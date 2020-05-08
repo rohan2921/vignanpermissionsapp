88bb4fdd289db91e5180a311744ed3b3c8a889b3
rohan2921 <37621549+rohan2921@users.noreply.github.com>
2020-05-08 22:19:51 +0530

    added authentication

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:vignanpermissions/screens/auth_screen.dart';
 
 import './screens/Events_screen.dart';
 
@@ -14,19 +16,31 @@ class _MyAppState extends State<MyApp> {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-        title: 'Flutter Demo',
-        theme: ThemeData(
-            primaryColor: Color.fromARGB(0xFF, 0x49, 0x48, 0x9D),
-            accentColor: Color.fromARGB(0xFF,0x6C,0x68,0xF5),//#84A5C60 xFF, 0xA7, 0xCE, 0xCB 6C68F5
-            accentColorBrightness: Brightness.dark,
-            backgroundColor: Colors.white,
-            primarySwatch: Colors.blue,
-            buttonTheme: ButtonTheme.of(context).copyWith(
-                buttonColor: Colors.blue,
-                textTheme: ButtonTextTheme.primary,
-                shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(20)))),
-        home: EventScreen() //start your widgets from here
-        );
+      title: 'Flutter Demo',
+      theme: ThemeData(
+          primaryColor: Color.fromARGB(0xFF, 0x49, 0x48, 0x9D),
+          accentColor: Color.fromARGB(
+              0xFF, 0x6C, 0x68, 0xF5), //#84A5C60 xFF, 0xA7, 0xCE, 0xCB 6C68F5
+          accentColorBrightness: Brightness.dark,
+          backgroundColor: Colors.white,
+          primarySwatch: Colors.blue,
+          buttonTheme: ButtonTheme.of(context).copyWith(
+              buttonColor: Colors.blue,
+              textTheme: ButtonTextTheme.primary,
+              shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(20)))),
+      home: StreamBuilder(
+          stream: FirebaseAuth.instance.onAuthStateChanged,
+          builder: (ctx, snapShot) {
+            //if(snapShot.connectionState==ConnectionState.waiting){
+            //  return SplashScreen();
+            // }
+            if (snapShot.hasData) {
+              return EventScreen();
+            } else {
+              return AuthScreen();
+            }
+          }), //start your widgets from here
+    );
   }
 }
